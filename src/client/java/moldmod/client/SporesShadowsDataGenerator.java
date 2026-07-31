278fcb739d0121c4b7b8490afe4f89abe0cd530a
rcky0802 <rcky0802@gmail.com>
2026-07-31 11:01:55 +0200

    Initial commit

---
+++ src/client/java/moldmod/client/SporesShadowsDataGenerator.java
@@ -0,0 +1,11 @@
+package moldmod.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SporesShadowsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
